af551bd813f11d4b6031299107fa64b1824c1ac4
Henry <henhen1227@gmail.com>
2023-10-10 20:07:56 -0400

    End of 10/10 Meeting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import LostItemsList from "./LostItemsList";
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [lostItems, setLostItems] = useState([]);
 
   const addNumbers = (a, b) => {
     return a + b;
@@ -11,12 +14,25 @@ function App() {
     return a + b;
   }
 
+  const updateLostItems = () => {
+      fetch('http://localhost:4004/lostItems')
+            .then(response => response.json())
+            .then(data => setLostItems(data.items));
+  }
+
+    useEffect(() => {
+        updateLostItems();
+    }, []);
+
 
   return (
     <div className="App">
         <h1>Hello World!</h1>
         <p>hi, { addNumbers(1, 2) }</p>
-        <LostItemsList lostItems={["Hello world", "Hi"]} />
+        {lostItems && lostItems.length === 0 && <p>There are no lost items.</p>}
+        {lostItems && lostItems.length > 0 &&
+            <LostItemsList lostItems={lostItems} />
+        }
     </div>
   );
 }
